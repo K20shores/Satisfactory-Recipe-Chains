fc039aaa7987a3d3821c1ddcba03535f30cb38cd
Kyle Shores <kyle.shores44@gmail.com>
2024-08-18 11:31:25 -0500

    making a graph component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <v-tabs v-model="tab" grow bg-color="secondaryContainer">
-      <v-tab color="onSecondaryContainer">Tree</v-tab>
+      <!-- <v-tab color="onSecondaryContainer">Tree</v-tab> -->
       <v-tab color="onSecondaryContainer">Items</v-tab>
       <v-tab color="onSecondaryContainer">Resources</v-tab>
       <v-tab color="onSecondaryContainer">Network</v-tab>
@@ -9,64 +9,76 @@
   </v-container>
   <v-container>
     <v-window v-model="tab">
-      <v-window-item value="Tree">
+      <!-- <v-window-item value="Tree">
         <v-treeview :items="tree"></v-treeview>
-      </v-window-item>
+      </v-window-item> -->
       <v-window-item value="Items">
         <v-row>
-          <v-col cols="12" sm="6" md="4" lg="3" xl="2" v-for="item in items" :key="item.name">
+          <v-col
+            cols="12"
+            sm="6"
+            md="4"
+            lg="3"
+            xl="2"
+            v-for="item in items"
+            :key="item.name"
+          >
             <v-card color="surface">
               <v-card-title color="onSurface">{{ item.name }}</v-card-title>
-              <v-card-text color="onSurface">{{ item.description }}</v-card-text>
+              <v-card-text color="onSurface">{{
+                item.description
+              }}</v-card-text>
             </v-card>
           </v-col>
         </v-row>
       </v-window-item>
       <v-window-item value="Resources">
         <v-row>
-          <v-col cols="12" sm="6" md="4" lg="3" xl="2" v-for="resource in resources" :key="resource.name">
+          <v-col
+            cols="12"
+            sm="6"
+            md="4"
+            lg="3"
+            xl="2"
+            v-for="resource in resources"
+            :key="resource.name"
+          >
             <v-card color="surface">
               <v-card-title color="onSurface">{{ resource.name }}</v-card-title>
-              <v-card-text color="onSurface">{{ resource.description }}</v-card-text>
+              <v-card-text color="onSurface">{{
+                resource.description
+              }}</v-card-text>
             </v-card>
           </v-col>
         </v-row>
       </v-window-item>
       <v-window-item value="Network">
-        <v-network-graph
-          class="graph"
-          :nodes="nodes"
-          :edges="edges"
-          style="margin: 0 auto;"
-        />
+        <v-container>
+          <RecipeGraph />
+        </v-container>
       </v-window-item>
     </v-window>
   </v-container>
 </template>
 
 <script>
-import data from '../assets/data.json';
+import data from "../assets/data.json";
+import RecipeGraph from '../components/RecipeGraph.vue'
 
 export default {
+  components: {
+    RecipeGraph
+  },
   data() {
     return {
-      tab: 'Tree',
+      tab: "Items",
       items: data.items,
       resources: data.resources,
       tree: data.tree,
-      nodes: data.graph.nodes,
-      edges: data.graph.edges,
     };
   },
 };
 </script>
 
 <style>
-.graph {
-  width: 800px;
-  height: 600px;
-  /* border: 1px solid rgba(0, 0, 0, 0.5); */
-  border-radius: 5%;
-  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
-}
 </style>
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,38 +1,23 @@
 <template>
   <v-container>
     <h1>Test</h1>
-    <v-network-graph
-      class="graph"
-      :nodes="nodes"
-      :edges="edges"
-    />
+    <RecipeGraph />
   </v-container>
 </template>
 
 <script>
+import RecipeGraph from '../components/RecipeGraph.vue'
+
 export default {
+  components: {
+    RecipeGraph
+  },
   data() {
     return {
-      nodes: {
-        node1: { name: "Node 1" },
-        node2: { name: "Node 2" },
-        node3: { name: "Node 3" },
-        node4: { name: "Node 4" },
-      },
-      edges: {
-        edge1: { source: "node1", target: "node2" },
-        edge2: { source: "node2", target: "node3" },
-        edge3: { source: "node3", target: "node4" },
-      }
     }
   }
 }
 </script>
   
 <style scoped>
-.graph {
-  width: 800px;
-  height: 600px;
-  border: 1px solid #000;
-}
 </style>

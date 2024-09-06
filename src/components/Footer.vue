46de2a6bdc8a65820a75a11dd55a12298b891897
Kyle Shores <kyle.shores44@gmail.com>
2024-09-06 11:51:38 -0500

    adding version and authoer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,25 +1,29 @@
 <template>
-    <v-footer color="secondary" style="max-height: 50px;">
-      <v-container fluid text-xs-center>
-        <v-row align="center">
-          <v-col cols="12">
-            <p color="onSecondary">© {{ currentYear }} James Hollas</p>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-footer>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Footer',
-    data() {
-      return {
-        currentYear: new Date().getFullYear(),
-      };
-    },
-  }
-  </script>
-  
-  <style scoped>
-  </style>
+  <v-footer color="secondary" style="max-height: 50px;">
+    <v-container fluid text-xs-center>
+      <v-row align="center">
+        <v-col cols="6">
+          <p color="onSecondary">© {{ currentYear }} James Hollas & Kyle Shores</p>
+        </v-col>
+        <v-col cols="6">
+          <p color="onSecondary" class="text-right">Version: {{ packageVersion }}</p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {
+      currentYear: new Date().getFullYear(),
+      packageVersion: import.meta.env.PACKAGE_VERSION,
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { version } from './package.json';
 
 export default defineConfig({
   plugins: [vue()],
   base: process.env.VITE_BASE_URL,
+  define: {
+    'import.meta.env.PACKAGE_VERSION': JSON.stringify(version),
+  },
 })
